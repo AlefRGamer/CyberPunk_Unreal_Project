7e3030d724843148282bc15729aa47a8be878025
Alef Ronaldo Palmeira dos Santos <alef.ronaldo158@gmail.com>
2017-01-12 18:17:23 -0300

    C++ visual Studio 2015
    Adição de codigo em c++ para criação da mecanica do personagem

---
+++ Source/CyberPunk_Unreal/CyberPunk_Unreal.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CyberPunk_Unreal.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CyberPunk_Unreal, "CyberPunk_Unreal" );
